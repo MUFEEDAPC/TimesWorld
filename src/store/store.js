0b67e4c0e6b8eeb70d054af7db5e0d3ea1364311
MUFEEDAPC <pcmufeeda@gmail.com>
2025-08-13 15:03:22 +0530

    feat: changes

--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authReducer from '../features/auth/authSlice.js'
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
+import authReducer from '../features/auth/authSlice.js';
+
+const persistConfig = {
+  key: 'auth',
+  storage,
+};
+
+const persistedAuthReducer = persistReducer(persistConfig, authReducer);
 
 const store = configureStore({
   reducer: {
-    auth: authReducer,
+    auth: persistedAuthReducer,
   },
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
 
-export default store 
+export const persistor = persistStore(store);
+export default store;
